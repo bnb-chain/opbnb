370596a6560bc84023e307ec0fddfa93d6e71f53
Welkin <welkin.b@nodereal.com>
2023-06-30 20:25:32 +0800

    ops-bedrock:modify the script to support fast start devnet

--- bedrock-devnet/devnet/__init__.py
+++ bedrock-devnet/devnet/__init__.py
@@ -7,6 +7,7 @@ import socket
 import calendar
 import datetime
 import time
+import requests
 import shutil
 
 import devnet.log_setup
@@ -113,7 +114,7 @@ def devnet_deploy(paths):
     run_command(['docker-compose', 'up', '-d', 'l1'], cwd=paths.ops_bedrock_dir, env={
         'PWD': paths.ops_bedrock_dir
     })
-    wait_up(8545)
+    wait_up_url("http://127.0.0.1:8545/",'{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":74}',"wait L1 up...")
 
     log.info('Generating network config.')
     devnet_cfg_orig = pjoin(paths.contracts_bedrock_dir, 'deploy-config', 'devnetL1.json')
@@ -132,7 +133,7 @@ def devnet_deploy(paths):
         run_command(['yarn', 'hardhat', '--network', 'devnetL1', 'deploy', '--tags', 'l1'], env={
             'CHAIN_ID': '900',
             'L1_RPC': 'http://localhost:8545',
-            'PRIVATE_KEY_DEPLOYER': 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+            'PRIVATE_KEY_DEPLOYER': '59ba8068eb256d520179e903f43dacf6d8d57d72bd306e1bd603fdb8c8da10e8'
         }, cwd=paths.contracts_bedrock_dir)
         contracts = os.listdir(paths.deployment_dir)
         addresses = {}
@@ -158,6 +159,16 @@ def devnet_deploy(paths):
     if os.path.exists(paths.genesis_l2_path):
         log.info('L2 genesis and rollup configs already generated.')
     else:
+        log.info('Generating network config.')
+        shutil.copy(devnet_cfg_orig, devnet_cfg_backup)
+        deploy_config = read_json(devnet_cfg_orig)
+        l1BlockTag = l1BlockTagGet()["result"]
+        print(l1BlockTag)
+        l1BlockTimestamp = l1BlockTimestampGet(l1BlockTag)["result"]["timestamp"]
+        print(l1BlockTimestamp)
+        deploy_config['l1GenesisBlockTimestamp'] = l1BlockTimestamp
+        deploy_config['l1StartingBlockTag'] = l1BlockTag
+        write_json(devnet_cfg_orig, deploy_config)
         log.info('Generating L2 genesis and rollup configs.')
         run_command([
             'go', 'run', 'cmd/main.go', 'genesis', 'l2',
@@ -183,7 +194,8 @@ def devnet_deploy(paths):
     run_command(['docker-compose', 'up', '-d', 'op-node', 'op-proposer', 'op-batcher'], cwd=paths.ops_bedrock_dir, env={
         'PWD': paths.ops_bedrock_dir,
         'L2OO_ADDRESS': addresses['L2OutputOracleProxy'],
-        'SEQUENCER_BATCH_INBOX_ADDRESS': rollup_config['batch_inbox_address']
+        'SEQUENCER_BATCH_INBOX_ADDRESS': rollup_config['batch_inbox_address'],
+        'OP_BATCHER_SEQUENCER_BATCH_INBOX_ADDRESS': rollup_config['batch_inbox_address'],
     })
 
     log.info('Devnet ready.')
@@ -217,6 +229,53 @@ def wait_up(port, retries=10, wait_secs=1):
 
     raise Exception(f'Timed out waiting for port {port}.')
 
+def wait_up_url(url,body,wait_msg):
+    status = True
+    print(wait_msg)
+    while status:
+        try:
+            headers = {
+                "Content-Type": "application/json"
+            }
+
+            response = requests.post(url, headers=headers, data=body)
+            if response.status_code != 200:
+                time.sleep(5)
+            else:
+                print("Status code is 200, continue next step")
+                status = False
+        except requests.exceptions.ConnectionError:
+                time.sleep(5)
+
+def l1BlockTagGet():
+    headers = {
+        "Content-Type": "application/json"
+    }
+    try:
+        response = requests.post("http://127.0.0.1:8545",headers=headers,data='{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":74}')
+        if response.status_code != 200:
+            print(f'l1BlockTagGet resp status code is not 200, is {response.status_code}')
+            raise Exception("l1BlockTagGet status not 200!")
+        else:
+            result=response.json()
+            print(result)
+            return result
+    except requests.exceptions.ConnectionError:
+        raise Exception("l1BlockTagGet connection fail")
+
+def l1BlockTimestampGet(block_tag):
+    headers = {
+            "Content-Type": "application/json"
+    }
+    try:
+        response = requests.post("http://127.0.0.1:8545",headers=headers,data=f'{{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["{block_tag}", false],"id":74}}')
+        if response.status_code != 200:
+            print(f'l1BlockTimestampGet resp status code is not 200, is {response.status_code}')
+            raise Exception("l1BlockTimestampGet status not 200!")
+        else:
+            return response.json()
+    except requests.exceptions.ConnectionError:
+        raise Exception("l1BlockTimestampGet connection fail")
 
 def write_json(path, data):
     with open(path, 'w+') as f:
--- packages/contracts-bedrock/hardhat.config.ts
+++ packages/contracts-bedrock/hardhat.config.ts
@@ -41,9 +41,7 @@ const config: HardhatUserConfig = {
     devnetL1: {
       live: false,
       url: 'http://localhost:8545',
-      accounts: [
-        'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
-      ],
+      accounts: [process.env.PRIVATE_KEY_DEPLOYER || ethers.constants.HashZero],
     },
     devnetL2: {
       live: false,
