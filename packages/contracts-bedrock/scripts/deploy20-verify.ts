949dfbfe7cbc30d615cad6d6f93906a169ec1d23
Welkin <welkin.b@nodereal.com>
2023-08-29 20:36:14 +0800

    deploy 20 token script

--- packages/contracts-bedrock/scripts/deploy20-verify.ts
+++ packages/contracts-bedrock/scripts/deploy20-verify.ts
@@ -7,32 +7,43 @@
 import hre from "hardhat";
 
 async function main() {
-  // const factoryContract = await hre.ethers.getContractAt("OptimismMintableERC20Factory",'0x4200000000000000000000000000000000000012');
-  // const tx = await factoryContract.createOptimismMintableERC20('0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee','Binance USD-fake','BUSDf')
-  // console.log(`${tx.hash}`)
-  // let optimismMintableERC20 = await hre.ethers.getContractFactory("OptimismMintableERC20");
-  // let tokenContract = optimismMintableERC20.attach(erc20ContractAddress)
-  // const name = await tokenContract.name()
-  // const symbol = await tokenContract.symbol()
-  //
-  // console.log(
-  //   `deployed to ${erc20ContractAddress}, ${name}, ${symbol}`
-  // );
-  try {
-    await hre.run("verify:verify", {
-      address:"0xe4f27b04cc7729901876b44f4eaa5102ec150265",
-      constructorArguments: ["0x4200000000000000000000000000000000000010","0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee","Binance USD-fake","BUSDf"],
-    });
-  } catch (e) {
-    const msg = (e as Error).message;
-    if (msg.includes('Contract source code already verified')) {
-      console.log(`Contract [0xe4f27b04cc7729901876b44f4eaa5102ec150265] source code already verified`);
-    } else {
-      throw e;
-    }
-  }
+  await oneToken('0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee', 'Binance USD-fake2', 'BUSDf2');
+  process.exit();
+  return
+
 }
 
+async function oneToken(anotherToken,name,symbol) {
+  const factoryContract = await hre.ethers.getContractAt("OptimismMintableERC20Factory", '0x4200000000000000000000000000000000000012');
+  const tx = await factoryContract.createOptimismMintableERC20(anotherToken,name,symbol)
+  console.log(`${tx.hash}`)
+
+  // 2. Listen for the events
+
+  const tokenAddress = await getTokenAddress(factoryContract); // Await the Promise to get the value
+  console.log("Token address:", tokenAddress);
+
+  try {
+    await hre.run("opbnb-verify", {
+      contractName: "OptimismMintableERC20",
+      contractAddress: tokenAddress,
+      constructorArguments: ['0x4200000000000000000000000000000000000010', anotherToken,name,symbol]
+    })
+  } catch (e) {
+    const msg = (e as Error).message;
+    console.log(`verify err ${msg}`);
+    throw e;
+  }
+}
+function getTokenAddress(factoryContract): Promise<any> {
+  return new Promise((resolve) => {
+    factoryContract.on("OptimismMintableERC20Created", (localToken, remoteToken, deployer) => {
+      console.log("Event received:", localToken, remoteToken, deployer);
+      console.log(`deployed to ${localToken}`);
+      resolve(localToken); // Resolve the Promise with the updated value
+    });
+  });
+}
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
