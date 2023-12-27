fd9a681e140f42dc3fb7328d9879a71e0b7e1d01
Welkin <welkin.b@nodereal.com>
2023-12-27 16:52:35 +0800

    rollback lru cache

--- op-node/sources/caching/cache.go
+++ op-node/sources/caching/cache.go
@@ -1,8 +1,6 @@
 package caching
 
 import (
-	"sync"
-
 	lru "github.com/hashicorp/golang-lru"
 )
 
@@ -13,11 +11,9 @@ type Metrics interface {
 
 // LRUCache wraps hashicorp *lru.Cache and tracks cache metrics
 type LRUCache struct {
-	m       Metrics
-	label   string
-	inner   *lru.Cache
-	lock    sync.Mutex
-	maxSize int
+	m     Metrics
+	label string
+	inner *lru.Cache
 }
 
 func (c *LRUCache) Get(key any) (value any, ok bool) {
@@ -36,55 +32,14 @@ func (c *LRUCache) Add(key, value any) (evicted bool) {
 	return evicted
 }
 
-func (c *LRUCache) Peek(key any) (value any, ok bool) {
-	defer c.lock.Unlock()
-	c.lock.Lock()
-	return c.inner.Peek(key)
-}
-
-func (c *LRUCache) PeekAndCleanOld(key any) (value any, ok bool) {
-	defer c.lock.Unlock()
-	c.lock.Lock()
-	value, ok = c.inner.Peek(key)
-	if c.m != nil {
-		c.m.CacheGet(c.label, ok)
-	}
-	if ok {
-		for {
-			//Remove all elements older than the current element, as when retrieving that element, all elements older than it should have expired.
-			oKey, _, oOk := c.inner.GetOldest()
-			if oOk && oKey != key {
-				c.inner.RemoveOldest()
-				continue
-			}
-			break
-		}
-	}
-	return value, ok
-}
-
-func (c *LRUCache) AddIfNotFull(key, value any) (evicted bool, full bool) {
-	defer c.lock.Unlock()
-	c.lock.Lock()
-	if c.inner.Len() >= c.maxSize {
-		return false, true
-	}
-	evicted = c.inner.Add(key, value)
-	if c.m != nil {
-		c.m.CacheAdd(c.label, c.inner.Len(), evicted)
-	}
-	return evicted, false
-}
-
 // NewLRUCache creates a LRU cache with the given metrics, labeling the cache adds/gets.
 // Metrics are optional: no metrics will be tracked if m == nil.
 func NewLRUCache(m Metrics, label string, maxSize int) *LRUCache {
 	// no errors if the size is positive
 	cache, _ := lru.New(maxSize)
 	return &LRUCache{
-		m:       m,
-		label:   label,
-		inner:   cache,
-		maxSize: maxSize,
+		m:     m,
+		label: label,
+		inner: cache,
 	}
 }
