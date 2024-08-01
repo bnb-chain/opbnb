8f7737ab0d4455ccaa2f3e1a0496edaafcafa7dc
welkin22 <xwmbxx@gmail.com>
2024-08-01 10:19:47 +0800

    fix e2e case

--- op-e2e/setup.go
+++ op-e2e/setup.go
@@ -923,6 +923,7 @@ func configureL1(rollupNodeCfg *rollupNode.Config, l1Node EthInstance) {
 		BatchSize:        20,
 		HttpPollInterval: time.Millisecond * 100,
 		MaxConcurrency:   10,
+		CacheSize:        1000,
 	}
 }
 
