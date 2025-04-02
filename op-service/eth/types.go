2f55c3288fc6e5697de81b83e9dae7c13ac1877d
joey <10592664+joey0612@users.noreply.github.com>
2025-04-02 10:00:59 +0800

    fix: lint error

--- op-service/eth/types.go
+++ op-service/eth/types.go
@@ -5,7 +5,6 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"github.com/ethereum/go-ethereum/log"
 	"math"
 	"math/big"
 	"reflect"
@@ -15,6 +14,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/trie"
 	"github.com/holiman/uint256"
 )
